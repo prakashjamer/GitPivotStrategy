4dd7242cae0975cd2410381350d820dfc140363e
KMG <KMG@KMGKOL-LT034>
2026-09-12 20:12:08 +0530

    third commit

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace WebApplication1.Controllers
             messageHub = _messageHub;
         }
 
+
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -30,6 +32,22 @@ namespace WebApplication1.Controllers
             //offers.Add("25% Off on Samsung Smart TV");
             messageHub.Clients.All.SendOffersToUser("QQQQQQ"+ Guid.NewGuid().ToString());
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        public IEnumerable<WeatherForecast2> Getdata()
+        {
+            //List<string> offers = new List<string>();
+            //offers.Add("20% Off on IPhone 12");
+            //offers.Add("15% Off on HP Pavillion");
+            //offers.Add("25% Off on Samsung Smart TV");
+            messageHub.Clients.All.SendOffersToUser("QQQQQQ" + Guid.NewGuid().ToString());
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast2
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abf91d64839fd8f707fb28d2f4016a8b93d51fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fd45213bfd9d3c1a0f8bbb551a6e676db3303e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
